853ce31a4e8b1dde4acdf72a4bcc8d32e5cf13f5
aisbir Gans <75553444+aisbir@users.noreply.github.com>
2024-04-22 05:48:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 const { MongoClient } = require('mongodb');
 const mongoURI = 'mongodb+srv://bjirgann:aisbirgaming@cluster0.jqfnmva.mongodb.net/';
 const client = new MongoClient(mongoURI)
